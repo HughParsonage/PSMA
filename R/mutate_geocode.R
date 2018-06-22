a5b3c029d3fcc0bfd5615444d5f248b5e3e7d07e
HughParsonage <hugh.parsonage@gmail.com>
2018-06-22 19:40:08 +1000

    Add support for custom columns

--- R/mutate_geocode.R
+++ R/mutate_geocode.R
@@ -3,7 +3,14 @@
 #' @export
 #'
 
-mutate_geocode <- function(DT, new_names = c("lat", "lon"),
+mutate_geocode <- function(DT,
+                           flat_number = NULL,
+                           number_first = NULL,
+                           building_name = NULL,
+                           street_name = NULL,
+                           street_type = NULL,
+                           postcode = NULL,
+                           new_names = c("lat", "lon"),
                            overwrite = FALSE) {
   dt_was_tibble <- FALSE
   if (!is.data.table(DT)) {
@@ -16,6 +23,33 @@ mutate_geocode <- function(DT, new_names = c("lat", "lon"),
     }
     DT <- as.data.table(DT)
   }
+
+  if (flat_number_not_null <- !missing(flat_number)) {
+    old_flat_number <- as.character(substitute(flat_number))
+    setnames(DT, old_flat_number, "flat_number")
+  }
+  if (number_first_not_null <- !missing(number_first)) {
+    old_number_first <- as.character(substitute(number_first))
+    setnames(DT, old_number_first, "number_first")
+  }
+  if (building_name_not_null <- !missing(building_name)) {
+    old_building_name <- as.character(substitute(building_name))
+    setnames(DT, old_building_name, "building_name")
+  }
+  if (street_name_not_null <- !missing(street_name)) {
+    old_street_name <- as.character(substitute(street_name))
+    setnames(DT, old_street_name, "street_name")
+  }
+  if (street_type_not_null <- !missing(street_type)) {
+    old_street_type <- as.character(substitute(street_type))
+    setnames(DT, old_street_type, "street_type")
+  }
+  if (postcode_not_null <- !missing(postcode)) {
+    old_postcode <- as.character(substitute(postcode))
+    setnames(DT, old_postcode, "postcode")
+  }
+
+
   if (any(names(DT) %notin% c(names(formals(geocode)),
                               "attempt_decode_street_abbrev"))) {
     stop("DT requires the following names:\n\t",
@@ -23,6 +57,7 @@ mutate_geocode <- function(DT, new_names = c("lat", "lon"),
                         "attempt_decode_street_abbrev"),
                 sep = "\n\t"))
   }
+
 
   if (length(new_names) != 2L) {
     stop("`new_names` had length ", length(new_names), ", yet ",
@@ -47,6 +82,28 @@ mutate_geocode <- function(DT, new_names = c("lat", "lon"),
                               street_name = street_name,
                               street_type = street_type,
                               postcode = postcode)[, "ordering" := NULL]]
+
+  resetnames <- function(DT, a, b) {
+    setnames(DT, b, a)
+  }
+  if (flat_number_not_null) {
+    resetnames(DT, old_flat_number, "flat_number")
+  }
+  if (number_first_not_null) {
+    resetnames(DT, old_number_first, "number_first")
+  }
+  if (building_name_not_null) {
+    resetnames(DT, old_building_name, "building_name")
+  }
+  if (street_name_not_null) {
+    resetnames(DT, old_street_name, "street_name")
+  }
+  if (street_type_not_null) {
+    resetnames(DT, old_street_type, "street_type")
+  }
+  if (postcode_not_null) {
+    resetnames(DT, as.character(substitute(postcode)), "postcode")
+  }
   DT[]
 }
 
